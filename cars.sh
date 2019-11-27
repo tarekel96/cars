d07cdbb32d8aa1328a569166b9e208a7ca5f2115
Tarek El-Hajjaoui <tarekel96@gmail.com>
2019-11-27 10:25:07 -0800

    removed extra quotes for year make and model variables

--- cars.sh
+++ cars.sh
@@ -7,14 +7,14 @@ while true; do
 	type the number 1 to enter a new car,
 	type the number 2 to display the list of cars,
 	type the number 3 to quit and exit the program: "; read number
-	case "$number" in
+	case $number in
 		"1") echo -n "Enter the year of the new car: "
 			read year
 		     echo -n "Enter the make of the car: "
 	      		read make
-	     	     echo -n "Enter the model of the "$year" "$make": "
+	     	     echo -n "Enter the model of the $year $make: "
 	      		read model
-			echo -n "$year":"$make":"$model " >> ./My_old_cars;;
+			echo -n "$year:$make:$model " >> ./My_old_cars;;
 		"2") echo "Your old cars: " 
 			cat My_old_cars;;
 		"3") echo "Quit and exit: "
