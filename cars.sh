930f7cf897a8c290ac1ed185e4fbeebe1b1c1bbd
Tarek El-Hajjaoui <tarekel96@gmail.com>
2019-11-26 18:12:28 -0800

    implemented a while loop that encapsulates my previous case logic in order to keep the program running until the user enters 3 when prompted

--- cars.sh
+++ cars.sh
@@ -2,19 +2,22 @@
 # cars.sh
 # Tarek El-Hajjaoui
 
-echo -n "type the number 1 to enter a new car,
-type the number 2 to display the list of cars,
-type the number 3 to quit and exit the program: "; read number
-case "$number" in
-	"1") echo -n "Enter the make of the new car: "
-	      read make
-      	     echo -n "Enter the model of the "$make": "
-	      read model
-	     echo -n "Enter the year of the "$make" "$model": "
-	      read year
-	     echo -n "$year":"$make":"$model " >> ./My_old_cars;;
-	"2") echo "Your old cars: " 
-		cat My_old_cars;;
-	"3") echo "Quit and exit: "
-	       	exit ;;
-esac
+while true; do
+	echo -n "
+	type the number 1 to enter a new car,
+	type the number 2 to display the list of cars,
+	type the number 3 to quit and exit the program: "; read number
+	case "$number" in
+		"1") echo -n "Enter the year of the new car: "
+			read year
+		     echo -n "Enter the make of the car: "
+	      		read make
+	     	     echo -n "Enter the model of the "$year" "$make": "
+	      		read model
+			echo -n "$year":"$make":"$model " >> ./My_old_cars;;
+		"2") echo "Your old cars: " 
+			cat My_old_cars;;
+		"3") echo "Quit and exit: "
+	       		exit ;;
+	esac
+done
